c52ab17826ef0469dfecd94f7c36129c19ba5bc3
Alex Flood <alexalexflood@gmail.com>
2022-10-25 13:24:33 +0100

    Turned into effective console app

---
+++ Banking Console App/TestConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var input = Console.ReadLine();
+
+System.Console.WriteLine("You said " + input);
+
+Console.ReadKey();
