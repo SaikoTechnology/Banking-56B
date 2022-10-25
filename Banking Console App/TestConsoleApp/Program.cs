f0bc31da50071adf80d8ae01aaffa1165dbe90d9
Alex Flood <alexalexflood@gmail.com>
2022-10-25 13:41:10 +0100

    Fixed file location

--- Banking Console App/TestConsoleApp/Program.cs
+++ Banking Console App/TestConsoleApp/Program.cs
@@ -1,7 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
 Console.WriteLine("Hello, World!");
 var input = Console.ReadLine();
 
 System.Console.WriteLine("You said " + input);
 
 Console.ReadKey();
+
+
+
